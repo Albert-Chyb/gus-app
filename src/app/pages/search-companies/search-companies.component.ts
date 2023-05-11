1934e8aab9626d1337208c77ac6c49e64dfb7908
Albert-Chyb <albert7773000@gmail.com>
2023-05-12 00:48:00 +0200

    Replaced links with buttons that submit their form

--- src/app/pages/search-companies/search-companies.component.ts
+++ src/app/pages/search-companies/search-companies.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './search-companies.component.html',
@@ -8,4 +9,14 @@ export class SearchCompaniesComponent {
   nip = '';
   regon = '';
   krs = '';
+
+  constructor(private readonly router: Router) {}
+
+  runSearch(
+    queryParams: { nip: string } | { regon: string } | { krs: string }
+  ) {
+    const routerLink = 'company';
+
+    return this.router.navigate([routerLink], { queryParams });
+  }
 }
