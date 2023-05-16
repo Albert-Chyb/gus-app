9f0df59aaf2ea63721c4ef9edbfd38836c3c4bd8
Albert-Chyb <albert7773000@gmail.com>
2023-05-16 21:51:40 +0200

    Removed unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service';
-import { SearchCompaniesService } from './services/search-companies.service';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +6,5 @@ import { SearchCompaniesService } from './services/search-companies.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(
-    private readonly searchCompanies: SearchCompaniesService,
-    private readonly auth: AuthService
-  ) {}
-
-  test() {
-    // this.searchCompanies.byNIP('5213003700').subscribe(console.log);
-    this.searchCompanies.byREGON('000001407').subscribe(console.log);
-    // this.searchCompanies.byKRS('0000030211').subscribe(console.log);
-  }
-
-  logout() {
-    this.auth.logout();
-  }
+  constructor() {}
 }
